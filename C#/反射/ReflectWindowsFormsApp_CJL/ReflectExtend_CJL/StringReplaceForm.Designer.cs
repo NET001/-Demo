68bdad32fbb1fcc5341262bfbe428effe4acff38
NET001 <34703969+NET001@users.noreply.github.com>
2021-09-27 18:20:43 +0800

    20210927

---
+++ C#/反射/ReflectWindowsFormsApp_CJL/ReflectExtend_CJL/StringReplaceForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace ReplacAllWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbQuery = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rtbReplace = new System.Windows.Forms.RichTextBox();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtbQuery
+            // 
+            this.rtbQuery.Location = new System.Drawing.Point(22, 24);
+            this.rtbQuery.Name = "rtbQuery";
+            this.rtbQuery.Size = new System.Drawing.Size(290, 277);
+            this.rtbQuery.TabIndex = 0;
+            this.rtbQuery.Text = "";
+            this.rtbQuery.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rtbQuery);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 307);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "搜索串";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rtbReplace);
+            this.groupBox2.Location = new System.Drawing.Point(346, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(328, 307);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "替换串";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // rtbReplace
+            // 
+            this.rtbReplace.Location = new System.Drawing.Point(22, 24);
+            this.rtbReplace.Name = "rtbReplace";
+            this.rtbReplace.Size = new System.Drawing.Size(290, 277);
+            this.rtbReplace.TabIndex = 0;
+            this.rtbReplace.Text = "";
+            this.rtbReplace.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnReplace.Location = new System.Drawing.Point(533, 325);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(141, 53);
+            this.btnReplace.TabIndex = 2;
+            this.btnReplace.Text = "替换";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 389);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "字符串全局替换,支持使用正则匹配";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbQuery;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox rtbReplace;
+        private System.Windows.Forms.Button btnReplace;
+    }
+}
+
---
+++ C#/反射/ReflectWindowsFormsApp_CJL/ReflectExtend_CJL/StringReplaceMenuTool.cs
@@ -0,0 +1,22 @@
+﻿using ReflectInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReflectExtend_CJL
+{
+    public class StringReplaceMenuTool : IMenuTool
+    {
+        public string FatherText => "工具";
+
+        public string Text => "字符串替换";
+
+        public void Operation(RichTextBox rtb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
