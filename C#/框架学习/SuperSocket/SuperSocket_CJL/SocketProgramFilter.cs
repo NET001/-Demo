1105d4cd3d47d832606e73922221b9f3e3c3f527
NET001 <34703969+NET001@users.noreply.github.com>
2021-09-29 18:08:33 +0800

    20210929

---
+++ C#/框架学习/SuperSocket/SuperSocket_CJL/SocketProgramFilter.cs
@@ -0,0 +1,35 @@
+﻿using SuperSocket.ProtoBase;
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace SuperSocketServer_CJL
+{
+    /// <summary>
+    /// 协议解析类
+    /// </summary>
+    public class SocketProgramFilter : IPipelineFilter<SocketMessage>
+    {
+        public IPackageDecoder<SocketMessage> Decoder { get; set; }
+
+        public IPipelineFilter<SocketMessage> NextFilter => this;
+
+        public object Context { get; set; }
+        //对协议进行解析
+        public SocketMessage Filter(ref SequenceReader<byte> reader)
+        {
+            var bts = reader.Sequence.ToArray();
+            //将流标记到结尾,不然会一直循环这个方法出不去
+            reader.TryRead(out _);
+            var json = Encoding.UTF8.GetString(bts);
+            SocketMessage result = JsonConvert.DeserializeObject<SocketMessage>(json);
+            return result;
+        }
+        public void Reset()
+        {
+
+        }
+    }
+}
