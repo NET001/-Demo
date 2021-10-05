874ff76d1460337e261c3bfb84fae315c195d196
NET001 <1422323984@qq.com>
2021-10-05 18:02:35 +0800

    20211005

---
+++ C#/反射/ReflectTest/ReflectObject/TestObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ReflectObject
+{
+    public class TestObject
+    {
+        public int Property1 { get; set; }
+        public string Property2 { get; set; }
+        public bool Property3 { get; set; }
+    }
+}
--- C#/反射/ReflectTest/ReflectTest/Program.cs
+++ C#/反射/ReflectTest/ReflectTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ReflectObject;
+using System;
+using System.Reflection;
 
 namespace ReflectTest
 {
@@ -6,8 +8,23 @@ namespace ReflectTest
     {
         static void Main(string[] args)
         {
-
-
+            //Assembly
+            Assembly assem = typeof(TestObject).Assembly;
+            Console.WriteLine("程序集名称:" + assem.FullName);
+            Console.WriteLine("名称:" + assem.GetName().Name);
+            Console.WriteLine("版本:" + assem.GetName().Version);
+            Console.WriteLine("程序集路径:" + assem.CodeBase);
+            var t = assem.GetName();
+            Console.WriteLine("Version:");
+            //Module
+            //ConstructorInfo
+            //MethodInfo
+            //FieldInfo
+            //EventInfo
+            //PropertyInfo
+            //ParameterInfo
+            //CustomAttributeData
+            Console.ReadKey();
         }
     }
 }
