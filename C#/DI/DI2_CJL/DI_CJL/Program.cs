8226c0d3579ae5a7251396c88203ab3a84954b1d
chenjianlong <1422323984@qq.com>
2021-10-22 18:01:03 +0800

    20211022

---
+++ C#/DI/DI2_CJL/DI_CJL/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DI_CJL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ServiceCollection为IServiceCollection的默认实现,最后的BuildServiceProvider方法构建了一个容器对象
+            ServiceProvider serviceProvider = new ServiceCollection()
+                .AddTransient<IFoo, Foo>()
+                .AddScoped<IBar, Bar>()
+                .AddSingleton<IBaz, Baz>()
+                .BuildServiceProvider();
+            //从容器中获取对象实例
+            IBaz baz = serviceProvider.GetService<IBaz>();
+
+
+            Console.ReadKey();
+        }
+    }
+    public interface IFoo { }
+    public interface IBar { }
+    public interface IBaz { }
+    public class Foo : IFoo { }
+    public class Bar : IBar { }
+    public class Baz : IBaz { }
+    public class Base : IDisposable
+    {
+        public Base() => Console.WriteLine($"An instance of{GetType().Name} is created.");
+
+        public void Dispose() => Console.WriteLine($"The instance of{GetType().Name} is dsposed.");
+    }
+
+
+}
