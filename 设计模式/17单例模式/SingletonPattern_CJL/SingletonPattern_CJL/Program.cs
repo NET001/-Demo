20d8bf5e3af4aff370af9df8195a957670d637ea
NET001 <34703969+NET001@users.noreply.github.com>
2021-11-16 22:43:57 +0800

    20211116

--- 设计模式/17单例模式/SingletonPattern_CJL/SingletonPattern_CJL/Program.cs
+++ 设计模式/17单例模式/SingletonPattern_CJL/SingletonPattern_CJL/Program.cs
@@ -8,9 +8,10 @@ namespace SingletonPattern_CJL
     {
         static void Main(string[] args)
         {
-            Demo1();
-            Demo2();
-            Demo3();
+            //Demo1();
+            //Demo2();
+            //Demo3();
+            Demo4();
             Console.Read();
         }
         /// <summary>
@@ -40,7 +41,6 @@ namespace SingletonPattern_CJL
         /// </summary>
         static void Demo3()
         {
-
             Singleton3 s1 = Singleton3.GetInstance();
             Singleton3 s2 = Singleton3.GetInstance();
             if (s1 == s2)
@@ -48,24 +48,13 @@ namespace SingletonPattern_CJL
                 Console.WriteLine("两个是同一个对象");
             }
         }
-
         static void Demo4()
         {
-            string RunCommand(string command)
+            Singleton4 s1 = Singleton4.GetInstance("key1");
+            Singleton4 s2 = Singleton4.GetInstance("key1");
+            if (s1 == s2)
             {
-                Process myProcess = new Process();
-                myProcess.StartInfo.FileName = "cmd.exe";
-                myProcess.StartInfo.UseShellExecute = false;
-                myProcess.StartInfo.RedirectStandardInput = true;
-                myProcess.StartInfo.RedirectStandardOutput = true;
-                myProcess.StartInfo.RedirectStandardError = true;
-                myProcess.StartInfo.CreateNoWindow = true;
-                myProcess.Start();
-                myProcess.StandardInput.WriteLine(command);
-                myProcess.StandardInput.WriteLine("exit");
-                myProcess.WaitForExit();
-
-                return myProcess.StandardOutput.ReadToEnd();
+                Console.WriteLine("两个是同一个对象");
             }
         }
     }
---
+++ 设计模式/17单例模式/SingletonPattern_CJL/SingletonPattern_CJL/Singleton4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern_CJL
+{
+    /// <summary>
+    /// 静态内部类懒汉模式同时线程安全
+    /// </summary>
+    public class Singleton4
+    {
+        private static class SingletonHolder
+        {
+            public static readonly Dictionary<string, Singleton4> instances = new Dictionary<string, Singleton4>()
+            {
+                ["key1"] = new Singleton4(),
+                ["key2"] = new Singleton4(),
+                ["key3"] = new Singleton4(),
+            };
+        }
+        private Singleton4()
+        {
+            Console.WriteLine("创建了Singleton4实例");
+        }
+        public static Singleton4 GetInstance(string key)
+        {
+            return SingletonHolder.instances.TryGetValue(key, out _) ? SingletonHolder.instances[key] : null;
+        }
+    }
+}
