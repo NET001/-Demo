bccc07039db96f55ab3645f08ed445ada20f1551
chenjianlong <1422323984@qq.com>
2021-12-01 17:40:10 +0800

    20211201

--- 设计模式/10观察者模式/ObserverPattern_CJL/ObserverPattern_CJL/Program.cs
+++ 设计模式/10观察者模式/ObserverPattern_CJL/ObserverPattern_CJL/Program.cs
@@ -6,7 +6,23 @@ namespace ObserverPattern_CJL
     {
         static void Main(string[] args)
         {
-            Demo2();
+            EventBus eventBus = new EventBus();
+            Concrete1Observer concrete11Observer = new Concrete1Observer();
+            Concrete1Observer concrete12Observer = new Concrete1Observer();
+            Concrete2Observer concrete2Observer = new Concrete2Observer();
+            eventBus.Attach(concrete11Observer);
+            eventBus.Attach(concrete12Observer);
+            eventBus.Attach(concrete2Observer);
+            eventBus.Notify(new Subject1()
+            {
+                c1 = "1111",
+                c2 = "2222",
+            });
+            eventBus.Notify(new Subject2()
+            {
+                c3 = "3333",
+                c4 = "4444",
+            });
             Console.Read();
         }
         /// <summary>
@@ -38,7 +54,21 @@ namespace ObserverPattern_CJL
         /// </summary>
         static void Demo3()
         {
-
+            EventBus eventBus = new EventBus();
+            Concrete1Observer concrete1Observer = new Concrete1Observer();
+            Concrete2Observer concrete2Observer = new Concrete2Observer();
+            eventBus.Attach(concrete1Observer);
+            eventBus.Attach(concrete2Observer);
+            eventBus.Notify(new Subject1()
+            {
+                c1 = "1111",
+                c2 = "2222",
+            });
+            eventBus.Notify(new Subject2()
+            {
+                c3 = "3333",
+                c4 = "4444",
+            });
         }
     }
 }
--- 设计模式/7模板方法模式/TemplatePattern_CJL/TemplatePattern_CJL/Program.cs
+++ 设计模式/7模板方法模式/TemplatePattern_CJL/TemplatePattern_CJL/Program.cs
@@ -6,12 +6,36 @@ namespace TemplatePattern_CJL
     {
         static void Main(string[] args)
         {
+            Demo1();
+            Console.Read();
+        }
+        /// <summary>
+        /// 通过继承的方式
+        /// </summary>
+        static void Demo1()
+        {
             AbstractClass c;
             c = new ConcreteClassA();
             c.TemplateMethod();
             c = new ConcreteClassB();
             c.TemplateMethod();
-            Console.Read();
+        }
+        /// <summary>
+        /// 通过委托回调的方式
+        /// </summary>
+        static void Demo2()
+        {
+            CallBackTemplateClass callBackTemplateClass = new CallBackTemplateClass();
+            callBackTemplateClass.TemplateMethod((string str) =>
+            {
+                return str + "000";
+            });
+        }
+        static void Demo3()
+        {
+            CompositionTemplateA compositionTemplateA = new CompositionTemplateA();
+            CompositionTemplateClass compositionTemplateClass = new CompositionTemplateClass(compositionTemplateA);
+            compositionTemplateClass.TemplateMethod();
         }
     }
 }
