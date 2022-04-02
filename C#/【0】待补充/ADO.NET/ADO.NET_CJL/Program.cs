2a6668a33a7b165e6b45489d598f594bcb5b0323
chenjianlong <1422323984@qq.com>
2022-04-02 08:41:10 +0800

    20220402

---
+++ C#/【0】待补充/ADO.NET/ADO.NET_CJL/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ADO.NET_CJL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        //mysql
+        void Demo1()
+        {
+            string connectString = "";
+            MySqlConnection mySqlConnection = new MySqlConnection(connectString);
+            if (mySqlConnection.State != ConnectionState.Open)
+            {
+                mySqlConnection.Open();
+            }
+            string sql = "";
+            //查询
+            MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter(sql, mySqlConnection);
+            DataTable dt=new DataTable();
+            mySqlDataAdapter.Fill(dt);
+            //非查询
+            MySqlCommand mySqlCommand = new MySqlCommand(sql, mySqlConnection);
+            mySqlCommand.ExecuteNonQuery();
+        }
+    }
+}
--- C#/文件操作/IO_CJL/IO_CJL/Program.cs
+++ C#/文件操作/IO_CJL/IO_CJL/Program.cs
@@ -2,12 +2,20 @@
 using Microsoft.Extensions.Primitives;
 using System;
 using System.Reflection;
+using System.IO;
 
 namespace IO_CJL
 {
     class Program
     {
         static void Main(string[] args)
+        {
+
+        }
+        /// <summary>
+        /// 文件系统
+        /// </summary>
+        static void Demo1()
         {
             //读取指定物力存档的路径
             IFileProvider physicalFileProvider = new PhysicalFileProvider(@"E:\CJL_学习\Dome\C#\文件操作\IO_CJL");
@@ -16,12 +24,15 @@ namespace IO_CJL
             IFileProvider EmbeddedFileProvider = new EmbeddedFileProvider(Assembly.LoadFrom(""));
 
             //对文件进行监控
-            ChangeToken.OnChange(() => physicalFileProvider.Watch(""), () => {
+            ChangeToken.OnChange(() => physicalFileProvider.Watch(""), () =>
+            {
                 Console.WriteLine("文件状态发送了变化");
             });
-
-            
-
+        }
+        //获取bin目录
+        static void Demo2()
+        {
+            Directory.GetCurrentDirectory();
         }
     }
 }
